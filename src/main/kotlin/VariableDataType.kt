6c00918363662a6ec733d76bec332f83fffbab3e
amarnath-nitt <amarnath97.nitt@gmail.com>
2023-02-13 22:01:35 +0530

    added variable and data types

---
+++ src/main/kotlin/VariableDataType.kt
@@ -0,0 +1,9 @@
+fun main(args : Array<String>) {
+    val pi = 3.14;  // val can not be change once defined.
+    var r = 7; // var can be changed after defined
+
+    var area = pi * r * r;
+
+    println("Area of circle of radius ${r} is ${area}")
+
+}

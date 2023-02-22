017d1f1f6f19ee88bab4456c4d10eac1624c0073
amarnath-nitt <amarnath97.nitt@gmail.com>
2023-02-22 21:53:56 +0530

    added arithmetic operation

---
+++ src/main/kotlin/ArithmeticOperation.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var i = 15
+    var j = 4
+
+    println(i + j) // 19
+    println(i - j) // 11
+    println(i * j) // 60
+    println(i.toFloat() / j) // 3.75
+
+    if(i > j){
+       println("${i} is greater than ${j}")
+    }
+    else{
+       println("${j} is greater than ${i}")
+    }
+}
